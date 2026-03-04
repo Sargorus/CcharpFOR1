f3f0bac98db34107557899e443471961ede54d4e
Sargorus <Dima35206@yandex.ru>
2026-03-04 09:15:36 +0800

    Добавлен файл с тестами. Тесты пока не добавлены.

---
+++ C#FOR1Tests/LogicTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using C_FOR1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_FOR1.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void CompareMonthWhenIncreaseExceedsTest()
+        {
+            Assert.Fail();
+        }
+
+
+
+        [TestMethod()]
+        public void CompareMonthsExceedSumTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
