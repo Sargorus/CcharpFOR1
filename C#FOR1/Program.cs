8ca7b0a174b22766a2fc0d88bdc91a3b197dda86
Sargorus <Dima35206@yandex.ru>
2026-03-04 08:43:48 +0800

    Создан проект. Из исходников https://github.com/Sargorus/CsharpIF1 сделан скелет main.

--- C#FOR1/Program.cs
+++ C#FOR1/Program.cs
@@ -1,10 +1,45 @@
 ﻿namespace C_FOR1
 {
-    internal class Program
+    public class Logic
+    {
+        public static string CompareTrainInStation(int ArrivalHour, int ArrivalMinute, int DepartureHour, int DepartureMinute, int PassengerHour, int PassengerMinute)
+        {
+            int TimeArrive = ArrivalHour * 60 + ArrivalMinute;
+            int TimeDeparture = DepartureHour * 60 + DepartureMinute;
+            int TimePassenger = PassengerHour * 60 + PassengerMinute;
+
+            if (TimeArrive >= TimeDeparture) return "Время прибытия позже времени отбытия или равно ему!";
+            if (TimePassenger < TimeDeparture && TimePassenger > TimeArrive)
+            {
+                return "Поезд находится на станции";
+            }
+            else
+            {
+                return "Поезд не находится на станции";
+            }
+
+        }
+
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Введите начальную сумму вклада:");
+            var InitialDeposit = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите число В. где число В это порог для ежемесячного увеличения вклада");
+            var LimitInterestAccruals = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите число C, где С это через сколько месяцев размер вклада превысит C руб.");
+            var LimitDeposit = double.Parse(Console.ReadLine());
+
+            // var monthIncrease = Logic.CompareTrainInStation(ArrivalHour, ArrivalMinute, DepartureHour, DepartureMinute, PassengerHour, PassengerMinute);
+            // var monthsExeceed = Logic.
+
+            // Console.WriteLine(resultMSG);
+            // 
         }
     }
 }
